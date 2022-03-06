09eacbff16930a82c27ca5aa41ed56ccd355f512
andrericardoweb <andrericardoweb@gmail.com>
2022-03-06 20:31:42 -0300

    header e home

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,11 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
